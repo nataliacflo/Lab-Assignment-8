a1ff42f67025903c9281e9d1fee6ed546a0c389c
Natalia Flores <nataliaflores@Natalias-MacBook-Pro-2.local>
2019-05-13 20:57:13 -0700

    completed lab assignment 8

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Contact } from './contact.model';
 import { HttpClient } from '@angular/common/http';
+import { LocalStorageService } from '../localStorageService';
+import { ActivatedRoute } from '@angular/router';
+import { IUser } from '../login/login.component';
+import { Router } from '@angular/router';
+import { ToastService } from '../toast/toast.service';
 
 @Component({
   selector: 'contact',
@@ -10,31 +15,54 @@ import { HttpClient } from '@angular/common/http';
 export class ContactComponent implements OnInit {
 
   contacts: Array<Contact> = [];
-  contactParams: string = '';
-  constructor(private http: HttpClient) { }
+  contactParams = '';
+  localStorageService: LocalStorageService<Contact>;
+  currentUser: IUser;
+
+  constructor(
+    private http: HttpClient,
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+    private toastService: ToastService
+  ) {
+    this.localStorageService = new LocalStorageService('contacts');
+  }
 
   async ngOnInit() {
-    this.contacts = await this.loadItemsFromFile();
+    const currentUser = this.localStorageService.getItemsFromLocalStorage('user');
+    if (currentUser == null) {
+      this.router.navigate(['login']);
+    }
     this.loadContacts();
+    this.activatedRoute.params.subscribe((data: IUser) => {
+      this.currentUser = data;
+    });
   }
   async loadContacts() {
     const savedContacts = this.getItemsFromLocalStorage('contacts');
     if (savedContacts && savedContacts.length > 0) {
       this.contacts = savedContacts;
     } else {
-      this.contacts = await this.loadItemsFromFile();
+      const contacts = await this.loadItemsFromFile();
     }
     this.sortByID(this.contacts);
   }
 
   async loadItemsFromFile() {
-    const data = await this.http.get('assets/contacts.json').toPromise();
+    const data: any = await this.http.get('assets/contacts.json').toPromise();
     return data;
 
   }
 
   addContact() {
-    this.contacts.unshift(new Contact({}));
+    this.contacts.unshift(new Contact({
+      id: null,
+      firstName: null,
+      lastName: null,
+      phone: null,
+      email: null
+    }));
+      
   }
 
   deleteContact(index: number) {
@@ -43,19 +71,31 @@ export class ContactComponent implements OnInit {
   }
 
   saveContact(contact: any) {
-    contact.editing = false;
-    this.saveItemsToLocalStorage(this.contacts);
+    let hasError = false;
+    Object.keys(contact).forEach((key: any) => {
+      if (contact[key] === null) {
+        hasError = true;
+        this.toastService.showToast('danger', 2000, `Saved failed! Property ${key} must not be null!`);
+      }
+    });
+    if (!hasError) {
+      contact.editing = false;
+      this.saveItemsToLocalStorage(this.contacts);
+    } 
+
   }
 
   saveItemsToLocalStorage(contacts: Array<Contact>) {
     contacts = this.sortByID(contacts);
-    const savedContacts = localStorage.setItem('contacts', JSON.stringify(contacts));
-    return savedContacts;
+    return this.localStorageService.saveItemsToLocalStorage(contacts);
+    // const savedContacts = localStorage.setItem('contacts', JSON.stringify(contacts));
+    // return savedContacts;
   }
 
   getItemsFromLocalStorage(key: string) {
     const savedContacts = JSON.parse(localStorage.getItem(key));
-    return savedContacts;
+    return this.localStorageService.getItemsFromLocalStorage();
+    // return savedContacts;
   }
 
   searchContact(params: string) {
@@ -74,6 +114,13 @@ export class ContactComponent implements OnInit {
       return prevContact.id > presContact.id ? 1 : -1;
     });
     return contacts;
+  }
+
+  logout() {
+    // clear localStorage
+    this.localStorageService.clearItemFromLocalStorage('user');
+    // navigate to login page
+    this.router.navigate(['']);
   }
 
 }
